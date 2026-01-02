85b8e0706249a7ca654655d9f4b773f3c80b59ae
Anas Ahmad <ahmadanas6662@gmail.com>
2026-01-02 22:39:44 +0530

    Add files via upload

---
+++ C PROJECT 11/ANAS_10183_code/code.c
@@ -0,0 +1,43 @@
+/*
+Q11. Palindromic String Analyzer
+Accept strings, check for palindromes, and reverse them.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[], char rev[])
+{
+    int i, len;
+    len = strlen(str);
+
+    for (i = 0; i < len; i++)
+    {
+        rev[i] = str[len - i - 1];
+    }
+    rev[len] = '\0';
+}
+
+int main()
+{
+    char str[100], rev[100];
+
+    printf("Enter a string: ");
+    gets(str); // (Allowed in college projects)
+
+    reverseString(str, rev);
+
+    printf("\nOriginal String : %s", str);
+    printf("\nReversed String : %s", rev);
+
+    if (strcmp(str, rev) == 0)
+    {
+        printf("\nResult : The string is a PALINDROME");
+    }
+    else
+    {
+        printf("\nResult : The string is NOT a PALINDROME");
+    }
+
+    return 0;
+}
---
+++ C PROJECT 11/ANAS_10183_code/sample_output.c
@@ -0,0 +1,12 @@
+/* Enter a string: madam
+
+Original String : madam
+Reversed String : madam
+Result : The string is a PALINDROME */
+
+/*Enter a string: hello
+
+Original String : hello
+Reversed String : olleh
+Result : The string is NOT a PALINDROME
+*/
